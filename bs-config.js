08d02c2a189d4411541a0af2aceaa34a3bfbb479
deb <lee025@gmail.com>
2020-02-07 14:03:50 -0500

    Package.json engines: node & npm

--- bs-config.js
+++ bs-config.js
@@ -1,10 +1,12 @@
 module.exports = function(bs) {
   return {
+    // port: process.env.PORT,
+    // files: ["./**/*.{html,htm,css,js}"],
     server: {
       middleware: {
         // overrides the second middleware default with new settings
-        1: require('connect-history-api-fallback')({
-          index: '/dist/index.html',
+        1: require("connect-history-api-fallback")({
+          index: "/dist/index.html",
           verbose: true
         })
       }
